df62afff967ec8b44bc7f367c0f134f76a738dd4
alexander <alexandersznunes@gmail.com>
2025-12-30 00:18:23 -0300

    Refactor heapsort implementation: remove unused min macro, fix swap function, and enhance heap management

--- sorting/nd3_heapsort.c
+++ sorting/nd3_heapsort.c
@@ -1,15 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
 
 // Function-like Macro definition
-#define min(a, b) (((a) < (b)) ? (a) : (b))
+#define PARENT(i) i / 2
+#define LEFT(i) 2 * i + 1
+#define RIGHT(i) 2 * i + 2
+
+#define ARR_SIZE 14
+
+typedef struct Heap
+{
+    int arr[ARR_SIZE];
+    size_t heap_size;
+} Heap;
+
+void swap(int *a, int *b)
+{
+    int *tmp = a;
+    int a = b;
+    int b = tmp;
+}
+
+void max_heapify(Heap *heap, int i)
+{
+    int left_child = LEFT(i);
+    int right_child = RIGHT(i);
+    int largest;
+    if (left_child <= heap->heap_size && heap->arr[left_child] > heap->arr[i])
+    {
+        largest = left_child;
+    }
+    else
+    {
+        largest = i;
+    }
+
+    if (right_child <= heap->heap_size && heap->arr[right_child] > heap->arr[largest])
+    {
+        largest = right_child;
+    }
+
+    if (largest != i)
+    {
+        swap(heap->arr[i], heap->arr[largest]);
+        max_heapify(heap, largest);
+    }
+}
+
+void build_max_heap(Heap heap, int i)
+{
+    heap.heap_size = ARR_SIZE;
+    for (int i = ARR_SIZE / 2; i > 0; i++)
+    {
+        max_heapify(&heap, i);
+    }
+}
 
 int main()
 {
 
-    // Given two number a and b
-    int a = 18, b = 76;
+    Heap heap;
+    heap.heap_size = ARR_SIZE;
 
-    printf("Minimum: %d", min(a, b));
+    for (int i = 0; i < ARR_SIZE; i++)
+    {
+        if (scanf("%d", &heap.arr[i]) != 1)
+        {
+            fprintf(stderr, "Invalid input. Exiting.\n\n");
+            return EXIT_FAILURE;
+        }
+    }
 
-    return 0;
+    for (int i = 0; i < ARR_SIZE; i++)
+    {
+        printf("%d | ", heap.arr[i]);
+    }
+    printf("\n\n");
+
+    return EXIT_SUCCESS;
 }
